4ca82ecd9d616d3aced62774266c5f4a77ba6fe4
dodolette123 <94716819+dodolette123@users.noreply.github.com>
2023-09-06 23:01:29 -0400

    Create main.py

---
+++ Python1/main.py
@@ -0,0 +1,25 @@
+import sys
+
+import pygame as pg
+clock = pg.time.Clock()
+pg.init()
+
+screen = pg.display.set_mode((300,300))
+red = (255,0,0)
+green = (0,255,0)
+random = (50,40,122)
+
+myRect = pg.Rect(0,150,50,50)
+speed = 5
+while True:
+  screen.fill(red)
+  if myRect[0] > 300:
+    speed = -5
+  if myRect[0] < 0:
+    speed = 5
+    
+  myRect[0] += speed
+  pg.draw.rect(screen,green,myRect)
+
+  clock.tick(30)
+  pg.display.flip()
