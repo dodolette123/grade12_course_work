4d1f7d715504115ed671eee2e4f78f8467eb38f2
dodolette123 <94716819+dodolette123@users.noreply.github.com>
2023-09-08 12:47:09 -0400

    Create mouse_buttons.py

---
+++ Python1/mouse_buttons.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame as pg
+clock = pg.time.Clock()
+pg.init()
+
+screen = pg.display.set_mode((300,300))
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+white = (255,255,255)
+random = (50,40,122)
+
+myRect = pg.Rect(0,150,50,50)
+speed = 5
+while True:
+  pg.event.pump()
+  mx,my = pg.mouse.get_pos()
+  L,M,R = pg.mouse.get_pressed()
+  if L == True:
+    screen.fill(red)
+  elif R == True:
+    screen.fill(blue)
+  else:
+    screen.fill(white)
+  print(L,M,R)
+  #print(mx,my)
+  clock.tick(30)
+  pg.display.flip()
