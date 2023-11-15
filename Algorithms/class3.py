c2444dd42d73ded7397316f87e1a8c6f3afd3624
dodolette123 <94716819+dodolette123@users.noreply.github.com>
2023-11-15 12:53:57 -0500

    Update class3.py

--- Algorithms/class3.py
+++ Algorithms/class3.py
@@ -52,3 +52,22 @@ def linear_search(search_list, target_value):
 tour_stops = linear_search(tour_locations, target_city)
 print(tour_stops)
 
+
+
+#Finding the Maximum Value
+test_scores = [88, 93, 75, 100, 80, 67, 71, 92, 90, 83]
+
+#Linear Search Algorithm
+def linear_search(search_list):
+  maximum_score_index = None
+  for idx in range(len(search_list)):
+    #print(search_list[idx])
+    if maximum_score_index is None or search_list[idx] > maximum_score_index:
+      maximum_score_index = search_list[idx]
+  return maximum_score_index
+# Function call
+highest_score = linear_search(test_scores)
+
+#Prints out the highest score in the list
+print(highest_score)
+
